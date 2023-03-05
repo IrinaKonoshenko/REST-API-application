620cb0cbe2c8837f916d9c5420567a444193d660
Irina Konoshenko <ayren.syssoyeva@gmail.com>
2023-03-05 17:52:08 +0600

    refactoring

--- models/contacts.js
+++ models/contacts.js
@@ -1,17 +1,4 @@
 const mongoose = require("mongoose");
-const Joi = require("joi");
-
-const contactValidateSchema = Joi.object({
-  name: Joi.string().alphanum().min(3).max(30).required(),
-  email: Joi.string()
-    .email({
-      minDomainSegments: 2,
-      tlds: { allow: ["com", "net", "ru"] },
-    })
-    .required(),
-  phone: Joi.number().required(),
-  favorite: Joi.boolean(),
-});
 
 const contactsSchema = new mongoose.Schema({
   name: {
@@ -32,36 +19,4 @@ const contactsSchema = new mongoose.Schema({
 
 const Contacts = mongoose.model("contacts", contactsSchema);
 
-const listContacts = async () => {
-  return Contacts.find({});
-};
-
-const getContactById = async (contactId) => {
-  return Contacts.findById(contactId);
-};
-
-const addContact = async (body) => {
-  return Contacts.create(body);
-};
-
-const removeContact = async (contactId) => {
-  return Contacts.findByIdAndDelete(contactId);
-};
-
-const updateContact = async (contactId, body) => {
-  return Contacts.findByIdAndUpdate(contactId, body);
-};
-
-const updateStatusContact = async (contactId, body) => {
-  return Contacts.findByIdAndUpdate(contactId, body);
-};
-
-module.exports = {
-  listContacts,
-  getContactById,
-  addContact,
-  removeContact,
-  updateContact,
-  updateStatusContact,
-  contactValidateSchema,
-};
+module.exports = Contacts;
